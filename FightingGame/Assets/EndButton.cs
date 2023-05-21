a107575f6279beb568ca95580771484465f89ab1
wiszou <102638597+wiszou@users.noreply.github.com>
2023-05-21 23:00:08 +0800

    Playable

---
+++ FightingGame/Assets/EndButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void pressRetry(){
+        StartCoroutine(Retrydelay());
+        Debug.Log("Start");
+    }
+
+    IEnumerator Retrydelay(){
+        Debug.Log("Retrying the game");
+        yield return new WaitForSeconds(.1f);
+        Debug.Log("New Game Started");
+        SceneManager.LoadScene(0);
+    }
+}
